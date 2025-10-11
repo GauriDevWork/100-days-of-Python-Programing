3bd7508a04119353cba421deace075269669ff33
Gauri <gaurikaushik2013@gmail.com>
2025-10-11 11:26:11 +0530

    Day 1 completed
    Day 1 completed

---
+++ Day 1/charlength.py
@@ -0,0 +1,3 @@
+username = input("What's your name? ")
+length = len(username)
+print(length)
---
+++ Day 1/day1-demo-Band-Name-Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to Day 1 of 100 Days of Code!. In this file I am creating a demo for Day 1. This will include printing to the console, variables, input and string manipulation.")
+city = input("What is the name of the city you grew up in? \n")
+pet = input("What is the name of your pet? \n")
+band = city + " " + pet
+print("Your band name could be " + band)
